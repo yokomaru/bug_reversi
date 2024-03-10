fb8a034ed0a48f8eda7ac6fc47cf2b8807b71900
yokomaru <cabochetoganlan@gmail.com>
2024-03-10 22:31:23 +0900

    fix:  target_pos.out_of_board? もまとめてif 文に追加

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -61,8 +61,9 @@ module ReversiMethods
   end
 
   def turn(board, target_pos, attack_stone_color, direction)
-    return false if target_pos.out_of_board?
-    return false if target_pos.stone_color(board) == attack_stone_color || target_pos.stone_color(board) == BLANK_CELL
+    if target_pos.out_of_board? || target_pos.stone_color(board) == attack_stone_color || target_pos.stone_color(board) == BLANK_CELL
+      return false
+    end
 
     next_pos = target_pos.next_position(direction)
     if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
